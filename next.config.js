54c347dda6c6f481af365733c3ce9ea0393f390c
muhammad0065 <muhammad.alhusin93@gmail.com>
2024-11-05 20:05:06 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/immobliss',
+}
+
+module.exports = nextConfig 
